34ac4aa2b148e067cb5644351e2c1bf3f4c6fbac
GSLiTW <coslove8866@gmail.com>
2021-12-20 17:05:52 +0800

    fix: pass api unit test

--- app/application/services/generate_text_cloud.rb
+++ app/application/services/generate_text_cloud.rb
@@ -8,33 +8,10 @@ module HeadlineConnector
     class GenerateTextCloud
       include Dry::Transaction
 
-      step :validate_topic
       step :generate_text_cloud
-      step: reify_textCloud
+      step :reify_textCloud
 
       private
-
-      def validate_topic(input)
-        input[:topic] = Repository::For.klass(Entity::Topic).find_topic_keyword(input[:keyword]) ? Success(input) : Failure('Topic not found')
-
-      rescue StandardError => error
-        puts error.backtrace.join("\n")
-        Failure('Please first request this project to be added to your list')
-      end
-
-      # request_video is used for generate_text_cloud method
-      def request_videos(input)
-        # Assume that all related videos are already in the database
-        input[:related_feeds_entities] = input[:topic].related_videos_ids.map do |video_id|
-          Repository::For.klass(Entity::Feed).find_feed_id(video_id)
-        end
-        
-        Success(input)
-        
-      rescue StandardError => error
-        puts error.backtrace.join("\n")
-        Failure('Having some troubles conducting request_videos() to the Youtube Api')
-      end
 
       def generate_text_cloud(input)
           input[:textcloud] = Mapper::TextCloudMapper.new(input[:related_feeds_entities]).generate_textcloud
@@ -53,6 +30,20 @@ module HeadlineConnector
           .then { |text_cloud| Success(text_cloud) }
       rescue StandardError
         Failure('Error in our text cloud generator -- please try again')
+      end
+
+      # Helper funtions
+      def request_videos(input)
+        # Assume that all related videos are already in the database
+        input[:related_feeds_entities] = input[:topic].related_videos_ids.map do |video_id|
+          Repository::For.klass(Entity::Feed).find_feed_id(video_id)
+        end
+        
+        Success(input)
+        
+      rescue StandardError => error
+        puts error.backtrace.join("\n")
+        Failure('Having some troubles conducting request_videos() to the Youtube Api')
       end
       
     end
