31ecf5fb2842d7e35d8aa6c0141131141302e21d
river1440-ce <river1440@gapp.nthu.edu.tw>
2022-01-06 10:02:32 +0800

    update: add fake_data.rb

---
+++ app/application/controllers/fake_data.rb
@@ -0,0 +1,33 @@
+headline_cluster = {
+    "Sports" => [{
+        article_url: "https://www.nytimes.com/2022/01/02/sports/hockey/canucks-cancer-manager-fan.html",
+        title: "Hockey Fan Spots Cancerous Mole at Game and Delivers a Lifesaving Note",
+        abstract: "Nadia Popovici wrote a message on her phone, with “mole,” “cancer” and
+        “doctor” in bright-red type to get the attention of Brian Hamilton, an assistant
+        equipment manager for the Vancouver Canucks."
+        # In nytimes results, the image is in results -> media -> type: image, choose format: mediumThreeByTwo440, whose height: 293, width: 440
+        img: "https://static01.nyt.com/images/2022/01/04/multimedia/02xp-canucks-print/02xp-canucks-mediumThreeByTwo440-v2.jpg",
+        tag: "Hockey, Ice"
+      }],
+    "Technology" => [{
+        article_url: "https://www.nytimes.com/2022/01/03/technology/wordle-word-game-creator.html",
+        title: "Wordle Is a Love Story",
+        abstract: "The word game has gone from dozens of players to hundreds of thousands
+        in a few months. It was created by a software engineer in Brooklyn for his partner."
+        img: "https://static01.nyt.com/images/2022/01/03/multimedia/03xp-wordle/03xp-wordle-mediumThreeByTwo440.png",
+        tag: "Computer and Video Games"
+        }, {
+        article_url: "https://www.nytimes.com/2022/01/03/technology/elizabeth-holmes-guilty.html",
+        title: "Elizabeth Holmes is found guilty of four counts of fraud.",
+        abstract: "The case against the founder of the failed blood testing start-up Theranos
+        came to symbolize the pitfalls of Silicon Valley’s culture of hustle and hype."
+        img: "https://static01.nyt.com/images/2022/01/03/multimedia/03holmes-guilty-sub/03theranos-liveblog-promo-mediumThreeByTwo440.jpg",
+        tag: "Frauds and Swindling"
+        }]
+  }
+  video_list = {
+      "This Week": ["ma67yOdMQfs", "wZYeAZtJUnE"],
+      "This month": ["A5oD6RhwCi0"],
+      "This Year": ["W7h-Yho8EB0"],
+      "Before this year": ["aS-NoUsMcac","3Ym2FjufYJM"]
+  }
